6a3ab80ad311ac57c5d4cb2bbfa2755ffdee5435
naisannovel <naisannovel@gmail.com>
2021-09-18 20:13:48 +0600

    update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -6,59 +6,33 @@ module.exports.inCart = async (req, res) => {
 
         const result = await CartModel.findOne({
             user: userId,
-            "myService.service": serviceId,
+            service: serviceId
+            // "myService.service": serviceId,
         });
         
         if (result) return res.status(400).send("service already exists in your Cart!");
         else res.send(result)
-
 }
 
-module.exports.addCart = async (req, res) => {
+module.exports.addCart = async (req,res)=>{
     const userId = req.user._id;
     const serviceId = req.params.id;
-    const userCartItem = await CartModel.findOne({
-        user: userId,
-    });
 
-    if (userCartItem !== null) {
-
-        try{
-            const item = await CartModel.updateOne({
-                user: userId,
-            }, {
-                $push: {
-                    myService: {
-                        service: serviceId,
-                    },
-                },
-            });
-            res.status(201).send('successfully added in your cart');
-        }catch(err){
-            res.status(400).send(err);
-        }
-    } else {
-        let cartItem = new CartModel({
-            user: userId,
-            myService: [{
-                service: serviceId,
-            }],
-        });
-        try {
-            const result = await cartItem.save();
-            res.status(201).send('successfully added in your cart');
-        } catch (err) {
-            res.status(400).send(err);
-        }
-    }
-};
+    let cartItem = new CartModel({ user: userId,service: serviceId });
+   try{
+    const result = await cartItem.save();
+    res.status(201).send(result);
+   }catch(err){
+       res.status(400).send(err)
+   }
+}
 
 module.exports.getCartItem = async (req, res) => {
     const cartItems = await CartModel.find({
             user: req.user._id,
         })
         .populate("user", "name email")
-        .populate("myService.service");
+        .populate("service");
     if(cartItems.length){
         return res.status(200).send(cartItems);
     }
@@ -68,27 +42,50 @@ module.exports.getCartItem = async (req, res) => {
 module.exports.getAllCartItem = async (req,res) => {
     const cartItems = await CartModel.find()
     .populate("user", "name email")
-    .populate("myService.service");
+    .populate("service");
     if(cartItems.length){
     return res.status(200).send(cartItems);
     }
     return res.status(200).send('don\'t take any service');
 };
 
-// module.exports.addCart = async (req,res)=>{
+
+
+// module.exports.addCart = async (req, res) => {
 //     const userId = req.user._id;
 //     const serviceId = req.params.id;
-//     const item = await CartModel.findOne({
+//     const userCartItem = await CartModel.findOne({
 //         user: userId,
-//         service: serviceId,
 //     });
-//     if (item) return res.status(400).send("service already exists in your Cart!");
 
-//     let cartItem = new CartModel({ user: userId,service: serviceId });
-//    try{
-//     const result = await cartItem.save();
-//     res.status(201).send(result);
-//    }catch(err){
-//        res.status(400).send(err)
-//    }
-// }
+//     if (userCartItem !== null) {
+
+//         try{
+//             const item = await CartModel.updateOne({
+//                 user: userId,
+//             }, {
+//                 $push: {
+//                     myService: {
+//                         service: serviceId,
+//                     },
+//                 },
+//             });
+//             res.status(201).send('successfully added in your cart');
+//         }catch(err){
+//             res.status(400).send(err);
+//         }
+//     } else {
+//         let cartItem = new CartModel({
+//             user: userId,
+//             myService: [{
+//                 service: serviceId,
+//             }],
+//         });
+//         try {
+//             const result = await cartItem.save();
+//             res.status(201).send('successfully added in your cart');
+//         } catch (err) {
+//             res.status(400).send(err);
+//         }
+//     }
+// };
--- models/cartModel.js
+++ models/cartModel.js
@@ -1,47 +1,45 @@
 const { Schema, model } = require('mongoose');
 
-const cartSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    myService: [{
-        service: {
-            type: Schema.Types.ObjectId,
-            ref: "Service",
-            required: true,
-        },
-        status: {
-            type: String,
-            enum: ['pending', 'ongoing', 'done'],
-            default: 'pending',
-            required: true
-        }
-    }],
-}, {
-    timestamps: true
-})
-
-module.exports.CartModel = model('Cart', cartSchema);
-
-
-
 // const cartSchema = new Schema({
 //     user: {
 //         type: Schema.Types.ObjectId,
 //         ref: "User",
 //         required: true
 //     },
-//     service: [{
-//         type: Schema.Types.ObjectId,
-//         ref: "Service",
-//         required: true, 
+//     myService: [{
+//         service: {
+//             type: Schema.Types.ObjectId,
+//             ref: "Service",
+//             required: true,
+//         },
+//         status: {
+//             type: String,
+//             enum: ['pending', 'ongoing', 'done'],
+//             default: 'pending',
+//             required: true
+//         }
 //     }],
-    // status:{
-    //     type: String,
-    //     enum:['pending','ongoing','done'],
-    //     default: 'pending',
-    //     required: true
-    // }
-// },{ timestamps: true })
+// }, {
+//     timestamps: true
+// })
+
+const cartSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    service: {
+        type: Schema.Types.ObjectId,
+        ref: "Service",
+        required: true, 
+    },
+    status:{
+        type: String,
+        enum:['pending','ongoing','done'],
+        default: 'pending',
+        required: true
+    }
+},{ timestamps: true })
+
+module.exports.CartModel = model('Cart', cartSchema);
