1242a1ed483ade4db8f55204798c850938d72b79
naisannovel <naisannovel@gmail.com>
2021-09-22 19:14:41 +0600

    update

--- googleAuthStrategy/googleAuthConfig.js
+++ googleAuthStrategy/googleAuthConfig.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:4001/api/auth/google/redirect'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
 }, async (accessToken, refreshToken, profile, cb) => {
     let user = await GoogleAuth.findOne({
         googleId:profile.id,
