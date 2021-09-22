1d9b06cc36bb0b9b6cf99896655f6d5953f7f258
naisannovel <naisannovel@gmail.com>
2021-09-23 04:14:08 +0600

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.MONGODB_SERVER_LOCAL}/car-service`)
+const DB = process.env.MAIN_MONGODB_SERVER.replace('<PASSWORD>',process.env.DB_PASSWORD);
+
+mongoose.connect(DB)
 .then(()=>console.log('mongodb connected successfully'))
 .catch(()=>console.log('mongodb connection failed'))
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 4001,()=>{
     console.log(`Listening on port ${process.env.PORT}`);
 })
