1f5ae9385add44d4760d3826dd62975b94b28c12
naisannovel <naisannovel@gmail.com>
2021-10-11 23:20:41 +0600

    update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -32,7 +32,7 @@ module.exports.getCartItem = async (req, res) => {
             user: req.user._id,
         })
         .populate("user", "name email")
-        .populate("service", "name");
+        .populate("service", "name price");
         return res.status(200).send(cartItems);
 };
 
